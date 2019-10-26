ee0e70c8b9b963a57a7126ed4bc72b590e868b59
dumingwei <dumingwei07050@hellobike.com>
2019-10-26 11:48:46 +0800

    使用channel与Flutter通信

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.androidapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import io.flutter.facade.Flutter
 import io.flutter.view.FlutterView
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.FrameLayout
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    val CHANNEL_NAME = "com.example.androidapp.channel_second"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +32,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun createView() {
         val flutterView: FlutterView = Flutter.createView(this, lifecycle, "route1")
+
+        /**
+         * 和flutter进行交互
+         */
+        MethodChannel(flutterView, CHANNEL_NAME).setMethodCallHandler(
+                object : MethodChannel.MethodCallHandler {
+                    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+                        Log.d(TAG, "onMethodCall: ")
+                        methodCall(call, result)
+                    }
+                }
+        )
         val layout =
                 FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 800)
         flContainer.addView(flutterView, layout)
+
     }
 
     private fun createFragment() {
@@ -36,4 +56,14 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.flContainer, Flutter.createFragment(null))
                 .commit()
     }
+
+    private fun methodCall(call: MethodCall, result: MethodChannel.Result) {
+        if (call.method == "open_second_activity") {
+            SecondActivity.launch(this)
+            result.success(true)
+        } else {
+            result.notImplemented()
+        }
+    }
+
 }
